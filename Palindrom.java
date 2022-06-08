bf41bbd74c224a02fb9ced1d0d9ec74282ffdbe9
zaferna <106475526+zaferna@users.noreply.github.com>
2022-06-08 23:10:45 +0300

    Add files via upload

---
+++ Palindrom.java
@@ -0,0 +1,30 @@
+
+    import java.util.Scanner;
+ public class Palindrom {
+        static boolean isPalindrom(int number){
+            int lastNumber,reverseNumber=0,tempNumber;
+            tempNumber = number;
+            while(tempNumber != 0){
+                lastNumber = tempNumber%10;
+                reverseNumber = (reverseNumber*10)+lastNumber;
+                tempNumber/=10;
+
+            }
+            if(reverseNumber==number)
+                return true;
+            else
+                return false;
+        }
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Sayý giriniz : ");
+            int number = scan.nextInt();
+            if (isPalindrom(number)){
+                System.out.println("Girdiðiniz sayý palindrom bir sayýdýr.");
+            }
+            else {
+                System.out.println("Girdiðiniz sayý palindrom bir sayý deðildir.");
+            }
+
+        }
+    }
